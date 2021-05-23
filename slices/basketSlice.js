10eed9eb3378346f876b3d0788becabcccfc3e8d
aninarafath6 <arafathanin6@gmail.com>
2021-05-23 20:56:53 +0530

    slices changes

--- slices/basketSlice.js
+++ slices/basketSlice.js
@@ -9,20 +9,23 @@ export const basketSlice = createSlice({
   initialState,
   reducers: {
     addToBasket: (state, action) => {
-      const index = state.items.findIndex((item) => item.id === action.payload.id)
+      const index = state.items.findIndex(
+        (item) => item.id === action.payload.id
+      );
       if (index >= 0) {
-        state.items[index].quantity = state.items[index].quantity +1
-        
+        state.items[index].quantity = state.items[index].quantity + 1;
       } else {
-         state.items = [...state.items, action.payload];
+        state.items = [...state.items, action.payload];
       }
     },
     removeFromBasket: (state, action) => {
-      const index = state.items.findIndex((item) => item.id === action.payload.id);
+      const index = state.items.findIndex(
+        (item) => item.id === action.payload.id
+      );
       let newBasket = [...state.items];
       if (index >= 0) {
         // exist item in basket
-        newBasket.splice(index,1);
+        newBasket.splice(index, 1);
       } else {
         console.warn(
           `cant remove item (id:${action.payload.id} as its not in cart)`
