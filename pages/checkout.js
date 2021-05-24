d057ab084c9395afade7bef427719cf872b4a047
aninarafath6 <arafathanin6@gmail.com>
2021-05-24 17:51:03 +0530

    cart design 2/2

--- pages/checkout.js
+++ pages/checkout.js
@@ -1,13 +1,17 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Image from "next/image";
 import Head from "next/head";
 import { useSelector } from "react-redux";
 import { selectItems } from "../slices/basketSlice";
 import CheckoutProduct from "../components/checkout product/CheckoutProduct";
-import Currency from "react-currency-formatter";  
+import Currency from "react-currency-formatter";
+import { signIn, signOut, useSession } from "next-auth/client";
 
 function Checkout() {
   const items = useSelector(selectItems);
+  const total = items.reduce((total, item) => total + item.price * 72, 0);
+  const [session] = useSession();
+
   return (
     <div className="bg-gray-100 p-0 m-0">
       <Head>
@@ -19,25 +23,20 @@ function Checkout() {
         <link rel="icon" href="/logo.png" />
       </Head>
 
-      <main className="lg:flex max-w-screen-2xl mx-auto bg-white ">
-        {/* left section  */}
-        <div className="flex-grow m-5 shadow-sm ">
-          <div className="hidden lg:flex">
-            <Image
-              src="/images/Prime-day-banner.png"
-              width={1020}
-              height={250}
-              objectFit="contain"
-              className=" "
-            />
+      <main className="lg:flex flex-row-reverse max-w-screen-xl mx-auto bg-white sm:bg-[#f3f4f6] rounded">
+        {/* top */}
+
+        <div className=" m-5 space-y-2 ">
+          <div className="bg-white p-1 flex items-center justify-center">
+            <img src="/ad.png" />
           </div>
-          {/* top */}
-          <div className=" border-none border border-b-2">
-            <h4 className="text-xl">
+
+          <div className="bg-white lg:text-center p-5 lg:h-52 rounded md:space-y-2">
+            <h4 className="text-xl ">
               {" "}
               Subtotal{" "}
               <span className="font-bold">
-                {<Currency quantity={25 * 72} currency="INR" />}
+                {<Currency quantity={total} currency="INR" />}
               </span>
             </h4>
             <p className="text-gray-600 text-sm">
@@ -51,15 +50,31 @@ function Checkout() {
               <span className="text-blue-400">Details</span>
             </p>
             <button className="bg-[#ffd814] w-full p-2 rounded-md  my-2 shadow-md active:shadow-none active:bg-white active:border border-1 active:border-[#ffd814]  focus:outline-none focus:ring-yellow-200">
-              Proceed to buy ({items.length} item)
+              {session
+                ? `Proceed to buy (${items.length} item)`
+                : `Sign In to buy (${items.length} item)`}
             </button>
           </div>
+        </div>
+        {/* left section  */}
+        <div className="flex-grow m-5 shadow-sm ">
+          <div className="hidden lg:flex bg-white py-3">
+            <Image
+              src="/images/Prime-day-banner.png"
+              width={1020}
+              height={250}
+              objectFit="contain"
+              className=" "
+            />
+          </div>
+
           <div className="flex flex-col space-y-10 p-5 bg-white">
             {items.map((item, i) => (
               <CheckoutProduct {...item} key={i} />
             ))}
           </div>
         </div>
+
         {/* right section  */}
         <div className=""></div>
       </main>
--- slices/basketSlice.js
+++ slices/basketSlice.js
@@ -53,4 +53,6 @@ export const { increment, removeFromBasket, decrement } = basketSlice.actions;
 // Selectors - This is how we pull information from the Global store slice
 export const selectItems = (state) => state.basket.items;
 
+
+
 export default basketSlice.reducer;
